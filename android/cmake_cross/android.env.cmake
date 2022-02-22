9d0dbfbe1f80adf144a56d9106203538fc7cf555
cuddje <363956347@qq.com>
2022-02-22 17:35:58 +0800

    Update android.env.cmake

--- android/cmake_cross/android.env.cmake
+++ android/cmake_cross/android.env.cmake
@@ -5,4 +5,4 @@ set(CMAKE_TOOLCHAIN_FILE ${CMAKE_ANDROID_NDK}/build/cmake/android.toolchain.cmak
 set(CMAKE_ANDROID_STL_TYPE gnustl_static)
 #set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 #set(CMAKE_ANDROID_ABI arm64-v8a)
-set(ANDROID_ABI x86_64)
+set(ANDROID_ABI x86_64) #https://developer.android.google.cn/ndk/guides/cmake#command-line_2 google文档中要求ANDROID_ABI
